46e6919169d599cfc0be87940b20731f939b5325
div98 <div98@mail.ru>
2018-09-18 08:32:39 -0400

    start commit

---
+++ 404.php
@@ -0,0 +1,3 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'application/Views/404.html';
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+define('ROOT',dirname(__FILE__).'/');
+define('APP',dirname(__FILE__).'/application/');
+//include IDEAL.'framework.php';
+//app::gi()->start();
+echo ROOT
+?>
