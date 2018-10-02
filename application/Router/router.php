9d3c652cf42a2ceef65ac71a7ba96aaf03f00ec6
bog5651 <div98@mail.ru>
2018-10-02 18:55:35 +0600

    index

--- application/Router/router.php
+++ application/Router/router.php
@@ -1,8 +1,22 @@
 <?php
-class Router
-{
-  function parse(){
-    
+class Router{
+ private static $instance = null;
+  
+ public $routes = null;
+  
+ public static function getInstance()
+  {
+    if(is_null(self::$instance))
+    {
+      self::$instance = new Router();
+    }
+    return self::$instance;
+  }
+  public static process()
+  {
+    $request = $_SERVER['REQUEST_URI'];
+    echo "<div>".$request."<div>"
+	echo APP."<BR>".ROOT
   }
 }
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
 define('ROOT',dirname(__FILE__).'/');
-define('APP',dirname(__FILE__).'/application/');
-//include IDEAL.'framework.php';
-//app::gi()->start();
-echo ROOT
+define('APP', ROOT.'application/');
+define('CORE', APP.'core/');
+
+$scope = glob(CORE.'*');
+foreach($scope as $file)
+{
+	require_once($file);
+}
+require_once(APP.'router.php');
+$router = Router::getInstance();
+$router->process();
+
 ?>
